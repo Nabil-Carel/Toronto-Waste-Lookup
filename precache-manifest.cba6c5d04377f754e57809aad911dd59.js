7d6f8ddac535e40c67ebf7657944bd28716a8b3e
Nabil-Carel <nabilgbaguidi8@gmail.com>
2019-01-19 19:10:31 -0500

    Updates

---
+++ precache-manifest.cba6c5d04377f754e57809aad911dd59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b776d414561a28164e32",
+    "url": "/Nabil-Carel/Toronto-Waste-Lookup/static/js/runtime~main.b776d414.js"
+  },
+  {
+    "revision": "587982fae6c078a6a3af",
+    "url": "/Nabil-Carel/Toronto-Waste-Lookup/static/js/main.587982fa.chunk.js"
+  },
+  {
+    "revision": "4d0db85e92e8eacdff6b",
+    "url": "/Nabil-Carel/Toronto-Waste-Lookup/static/js/1.4d0db85e.chunk.js"
+  },
+  {
+    "revision": "587982fae6c078a6a3af",
+    "url": "/Nabil-Carel/Toronto-Waste-Lookup/static/css/main.79097a70.chunk.css"
+  },
+  {
+    "revision": "0256256d9c2d3e5cdfecbcda11b08413",
+    "url": "/Nabil-Carel/Toronto-Waste-Lookup/index.html"
+  }
+];
